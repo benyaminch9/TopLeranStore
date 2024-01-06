cd792c6bb71ead8562c19dc0a5ef0aeeb483a0f7
Benyamin <145895687+benyaminch77@users.noreply.github.com>
2024-01-06 19:23:27 +0330

    Contine Project

---
+++ TopLearn/TopLearn.DataLayer/Context/TopLearnContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TopLearn.Web.Models.User;
+
+namespace TopLearn.DataLayer.Context
+{
+    public class TopLearnContext : DbContext
+    {
+        public TopLearnContext(DbContextOptions<TopLearnContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+    }
+}
--- TopLearn/TopLearn.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TopLearn.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
