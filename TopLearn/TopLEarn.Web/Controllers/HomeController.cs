d957584179f0d0b0fb59f92696373dcd7b5a5b9c
Benyamin <145895687+benyaminch77@users.noreply.github.com>
2024-01-06 19:59:45 +0330

    Cearte Models and Tables

---
+++ TopLearn/TopLEarn.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TopLEarn.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
